f7cfb52cac9d0d77cfe5df46f235e067261f9156
gbalaji <gbalaji@INgbalaji.local>
2014-09-04 16:17:00 +0530

    Added property files

--- src/main/java/com/poc/controllers/PostController.java
+++ src/main/java/com/poc/controllers/PostController.java
@@ -3,17 +3,17 @@ package com.poc.controllers;
 import com.poc.repository.PostRepository;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 public class PostController {
 
+    @Autowired
     private PostRepository repository;
-
 
     @RequestMapping("/")
     public String index() {
-
-        return "Greetings from Spring Boot@@@@@"+ repository.getPosts();
+        return "There are totally "+ repository.getPosts().size() + " posts";
     }
 
 }
--- src/main/java/com/poc/repository/PostRepository.java
+++ src/main/java/com/poc/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package com.poc.repository;
 
 import com.poc.models.Post;
+import javafx.geometry.Pos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -9,6 +10,8 @@ import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class PostRepository {
@@ -16,14 +19,19 @@ public class PostRepository {
     @Autowired
     protected JdbcTemplate jdbc;
 
-    public Post getPosts() {
-        return jdbc.queryForObject("SELECT * FROM post", itemMapper);
+    public List<Post> getPosts() {
+        return jdbc.queryForObject("SELECT * FROM post", POST_MAPPER);
     }
 
-    private static final RowMapper<Post> itemMapper = new RowMapper<Post>() {
-        public Post mapRow(ResultSet rs, int rowNum) throws SQLException {
-            Post post = new Post(rs.getInt("id"), rs.getString("name"));
-            return post;
+    private static final RowMapper<List<Post>> POST_MAPPER = new RowMapper<List<Post>>() {
+        public List<Post> mapRow(ResultSet rs, int rowNum) throws SQLException {
+            List<Post> postArrayList = new ArrayList<Post>();
+            while(rs.next()){
+                Post post = new Post(rs.getInt("id"), rs.getString("name"));
+                postArrayList.add(post);
+            }
+
+            return postArrayList;
         }
     };
 }
